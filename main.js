9bc983aaa057d5cac039decdcbbdc8f42994d9b8
thanhbi919@gmail.com <thanhbi919@gmail.com>
2022-07-05 17:48:35 +0700

    data binđing

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 var vueInstance = new Vue({
     el: '#app',
     data: {
-        title: 'Dien thoai Samsung',
+        title: 'Data Binding',
+        url: 'https://www.google.com',
+        target: '_blank',
+        price:10000
     },
     methods: {
         say:function(text){
-            return 'Hello'+text;
+            return 'Hello'+ text;
+        },
+        formatPrice(){
+            const number = this.price;
+
+           const price = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(number);
+            return price;
         }
     }
 });
 //Reactivity
 console.log(vueInstance);
 
-setTimeout(() => {
-    vueInstance.title = 'Dien thoai Nokia';
-},3000);
